3154d071cf2bacec9fed7463fc700bec73a69221
HugoCMU <hugo.p.cmu@gmail.com>
2023-04-21 11:12:31 -0500

    working end to end

--- ops.py
+++ ops.py
@@ -2,6 +2,8 @@ import os
 import shutil
 from io import BytesIO
 from typing import Dict, List, Union
+import random
+import uuid
 
 import google.auth
 from googleapiclient.discovery import build
@@ -18,11 +20,6 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 FONTS_DIR = os.path.join(ROOT_DIR, 'fonts')
 OUTPUT_DIR = os.path.join(ROOT_DIR, 'output')
-
-# Empty output directory (opinionated code motherfucker)
-if os.path.exists(OUTPUT_DIR):
-    shutil.rmtree(OUTPUT_DIR)
-os.mkdir(OUTPUT_DIR)
 
 # set google api key
 with open(os.path.join(ROOT_DIR, 'google.txt'), 'r') as f:
@@ -192,7 +189,6 @@ def stack_fgbg(
     # output image size,
     bg_size = (1280, 720),
     fg_size = (420, 420),
-    gaussian_mu_sig = ((0.05, 0.05), (0.5, 0.05)),
 ):
     # load images
     fg_image = Image.open(fg_image_path)
@@ -200,13 +196,16 @@ def stack_fgbg(
     # resize images
     fg_image = fg_image.resize(fg_size)
     bg_image = bg_image.resize(bg_size)
-    # Sample position for bu based on gaussian
-    x = int(np.random.normal(size=1, loc=gaussian_mu_sig[0][0], scale=gaussian_mu_sig[0][1])[0] * bg_size[0])
-    y = int(np.random.normal(size=1, loc=gaussian_mu_sig[1][0], scale=gaussian_mu_sig[1][1])[0] * bg_size[1])
-    # paste images
-    bg_image.paste(fg_image, (x, y), fg_image)
+    # Upper left corner of the foreground such that it sits in the lower left corner of background
+    x = 0
+    y = bg_size[1] - fg_size[1]
+    # Final image
+    fg_image_full = Image.new("RGBA", bg_size)
+    fg_image_full.paste(fg_image, (x, y), fg_image)
+    final = Image.alpha_composite(bg_image, fg_image_full)
+    # paste images, account for alpha channel
     # save output image
-    bg_image.save(output_path)
+    final.save(output_path)
 
 def get_video_info(video_id):
     if not video_id:
@@ -240,11 +239,153 @@ def get_video_sentence_from_description(description):
     text_before_like = parts[0].strip()
     return text_before_like
 
-if __name__ == '__main__':
-    pass
-    # remove_background()
-    # draw_text()
-    # stack_fgbg()
-    # gpt_text()
-    # for _ in range(5):
-    #     print(gpt_color())
+def generate_thumbnails(
+        input_image_path: str,
+        output_tmp_dir: str,
+        output_dir: str,
+        title: str,
+):
+    fg_prompt = gpt_text(
+        prompt="portrait of white bengal cat, blue eyes, cute, chubby",
+        system=' '.join([
+        "You generate variations of string prompts for image generation.",
+        "Respond with a single new variant of the user prompt. ",
+        "Add several new interesting or related words.",
+        "Respond with the prompt only: no extra text or explanations.",
+    ]),
+    )
+
+    # Foreground image remove background
+    fg_img_name = str(uuid.uuid4())
+    gpt_image(
+        prompt=fg_prompt,
+        output_path=os.path.join(output_tmp_dir, f'{fg_img_name}.png'),
+        image_size="512x512",
+    )
+    remove_bg(
+        image_path=os.path.join(output_tmp_dir, f'{fg_img_name}.png'),
+        output_path=os.path.join(output_tmp_dir, f'{fg_img_name}_nobg.png'),
+    )
+
+    # Background image 
+    bg_img_name = str(uuid.uuid4())
+    resize_bg(
+        image_path = input_image_path,
+        output_path = os.path.join(output_tmp_dir, f'{bg_img_name}.png'),
+        canvas_size = (1280, 720),
+    )
+
+    text_rgb, text_color = gpt_color()
+    rect_rgb, rect_color = gpt_color()
+
+    # Write text on top of image
+    draw_text(
+        image_path = os.path.join(output_tmp_dir, f'{bg_img_name}.png'),
+        output_path = os.path.join(output_tmp_dir,  f'{bg_img_name}_text.png'),
+        text = title,
+        text_color = text_rgb,
+        font_size = 60,
+        rectangle_color = rect_rgb,
+        rectangle_padding = 20,
+    )
+
+    # Stack foreground and background
+    combo_image_name = str(uuid.uuid4())
+    stack_fgbg(
+        fg_image_path = os.path.join(output_tmp_dir, f'{fg_img_name}_nobg.png'),
+        bg_image_path = os.path.join(output_tmp_dir, f'{bg_img_name}_text.png'),
+        output_path = os.path.join(output_dir, f'{combo_image_name}.png'),
+        bg_size = (1280, 720),
+        fg_size = (420, 420),
+    )
+
+def generate_yttext(
+    output_path: str,
+    desired_sentence: str,
+    example_video_ids: List[str],
+):
+
+    socials = '''
+Like 👍. Comment 💬. Subscribe 🟥.
+
+⌨️ GitHub
+https://github.com/hu-po
+
+🗨️ Discord
+https://discord.gg/XKgVSxB6dE
+
+📸 Instagram
+http://instagram.com/gnocchibengal
+    '''
+
+    best_videos = []
+    for video_id in example_video_ids:
+        title, description = get_video_info(video_id)
+        hashtags = get_video_hashtags_from_description(description)
+        sentence = get_video_sentence_from_description(description)
+        best_videos.append({
+            "title": title,
+            "hashtags": hashtags,
+            "sentence": sentence,
+        })
+
+    in_context_titles = []
+    for best_video in best_videos:
+        in_context_titles += [{
+            "role": "user",
+            "content": best_video["sentence"]
+        }]
+        in_context_titles += [{
+            "role": "assistant",
+            "content": best_video["title"],
+        }]
+    # Add the last part of the prompt
+    in_context_titles += [{
+        "role": "user",
+        "content": desired_sentence,
+    }]
+    title = gpt_text(
+        prompt=in_context_titles,
+        system=' '.join([
+            "You create titles for YouTube videos.",
+            "Respond with the title that best fits the description provided by the user.",
+            "Respond with the title only: no extra text or explanations.",
+        ]),
+        temperature=0.6,
+    )
+    
+
+    in_context_hashtags = []
+    for best_video in best_videos:
+        in_context_hashtags += [{
+            "role": "user",
+            "content": best_video["sentence"]
+        }]
+        in_context_hashtags += [{
+            "role": "assistant",
+            "content": best_video["hashtags"],
+        }]
+    # Add the last part of the prompt
+    in_context_hashtags += [{
+        "role": "user",
+        "content": desired_sentence,
+    }]
+    hashtags = gpt_text(
+        prompt=in_context_hashtags,
+        system=' '.join([
+            "You create hashtags for YouTube videos.",
+            "Respond with up to 4 hashtags that match the user prompt.",
+            "Respond with the hashtags only: no extra text or explanations.",
+        ]),
+        temperature=0.6,
+    )
+
+    # Combine all the parts together
+    full_description = f'{title}\n{desired_sentence}\n{socials}\n{hashtags}'
+
+    # Save the description
+    description_path = os.path.join(output_path, f'{str(uuid.uuid4())}.txt')
+    with open(description_path, 'w') as f:
+        f.write(full_description)
+
+    return title
